1b666254b1d781a3711727e179f5b0e3eb004321
tessjenkins <jenkinstess@wustl.edu>
2020-12-05 14:06:26 -0600

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <a href='http://ec2-54-236-53-54.compute-1.amazonaws.com:3456'>
+      <a href='http://localhost:3456'>
       <button>Login with Spotify</button>
       </a>
     </div>
