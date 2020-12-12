d07eb0a5c6d20a9a216727dda7b28c0b46492104
tessjenkins <jenkinstess@wustl.edu>
2020-12-12 15:42:19 -0600

    updates

--- auth-server/data_storage/app.js
+++ auth-server/data_storage/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-const app = express();
+const app2 = express();
 // a test route to make sure we can reach the backend
 //this would normally go in a routes file
-app.get('/test', (req, res) => {
+app2.get('/test', (req, res) => {
 res.send('Welcome to the backend!')
 })
 //Set the port that you want the server to run on
-const port = process.env.PORT || 3456;
-app.listen(port);
-console.log('App is listening on port ' + port);
+const port = process.env.PORT || 2345;
+app2.listen(port);
+console.log('App2 is listening on port ' + port);
