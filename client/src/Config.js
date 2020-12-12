b3cc0c1c9429c81675ff0b0e1552739880b08ea2
jamiesxu <59858397+jamiesxu@users.noreply.github.com>
2020-12-12 10:17:34 -0500

    firebase

---
+++ client/src/Config.js
@@ -0,0 +1,11 @@
+ const config = {
+ apiKey: "AIzaSyBKcQZHPr4FiTGuILZQQ469vLdh4Zna2Iw",
+    authDomain: "sharedspotify-9e17d.firebaseapp.com",
+    projectId: "sharedspotify-9e17d",
+    storageBucket: "sharedspotify-9e17d.appspot.com",
+    messagingSenderId: "1090673990726",
+    appId: "1:1090673990726:web:2cb2953118aab5ce5abe47",
+    measurementId: "G-GTLY4633PV"
+}
+
+export default config
